18cc525311f71f8b617847489a4085552e690c5d
Marco <marco@marco-ThinkPad-T61.(none)>
2012-10-06 21:00:50 +0200

    Solved problems with infinite loops and (most) duplicates

--- prolog/numbersets.pl
+++ prolog/numbersets.pl
@@ -1,96 +1,80 @@
 /* -*- mode: prolog -*- */
 
-% SEEMS to work for most cases that I've tried. 
-% However, returns a big amount of duplicates
-% and runs infinitely instead of failing if there
-% are no new cases for some predicates (in some modes).
+% Some problems with duplicates for union and intersection left.
 
-less(z,s(_)).
+less(z,s(X)):- num(X).
 less(s(X), s(Y)) :- less(X,Y).
+
 
 num(z).
 num(s(X)) :- num(X).
 
-equal(z,z).
-equal(s(X),s(Y)) :- equal(X,Y).
-
-equalList([],[]).
-equalList([H|T],[P|Q]) :- 
-	equal(H,P), 
-	equalList(T,Q).
-
-different(X,Y) :- less(X,Y).
-different(X,Y) :- less(Y,X).
-
-geq(X,Y):-equal(X,Y).
-geq(X,Y):-less(Y,X).
-
 checkset([]).
-checkset([_]).
-checkset([H,X]) :- less(H,X).
+checkset([X]):- num(X).
 checkset([A,B|C]) :- 
 	less(A,B), 
 	checkset([B|C]).
 
-ismember(_,[],no).
-ismember(X,[Y|Z],no) :- 
-	checkset([Y|Z]), 
-	different(X,Y), 
-	num(X), 
-	ismember(X,Z,no).
+
+ismember(X,Y,no):-
+	checkset([X|Y]),
+	checkset(Y).
+ismember(X,Y,no):-
+	checkset([Y|X]),
+	checkset(Y).
+ismember(X,[H|T],no):-
+	checkset([H,X|T]),
+	ismember(X,T,no),
+	checkset([H|T]).
+
+ismember(X,[X|Z],yes) :- 
+	checkset([X|Z]).
 ismember(X,[Y|Z],yes) :- 
 	checkset([Y|Z]), 
-	equal(X,Y), 
-	num(X).
-ismember(X,[Y|Z],yes) :- 
-	checkset([Y|Z]), 
-	num(X), 
 	ismember(X,Z,yes).
 
-allmembers([],_).
-allmembers([H|T],Y):-
-	ismember(H,Y,yes), 
-	allmembers(T,Y),
-	checkset([H|T]).
-
-insert(X,[],[X]).
-insert(X,[H|T],S) :- 
-	less(X,H), 
-	equalList([X,H|T],S),
-	checkset([H|T]).
-insert(X,[X|T],[X|T]) :- 
-	checkset([X|T]).
-insert(X,[H|T],[H|R]) :- 
-	less(H,X), 
-	insert(X,T,R), 
-	checkset([H|T]).
-
-
+% union([s(z)],Y,[z,s(z)]).
+% has duplicate of Y=[z,s(z)]
 union(X,[],X):- checkset(X).
 union([],X,X):- checkset(X).
-union([H],Y,S) :- 
-	insert(H,Y,S).
-union([H],Y,S) :- 
-	insert(H,Y,S).
-union([H|R],Y,S) :- 
-	insert(H,Y,T), 
-	union(R,T,S),
-	checkset(Y), 
-	checkset([H|R]),
-	checkset(S).
+union([H|R],[A|B],[H|T]) :- 
+	union(R,[A|B],T),
+	less(H,A),
+	checkset([A|B]), 
+	checkset([H|T]),
+	checkset([H|R]).
+union([A|B],[H|R],[H|T]) :- 
+	union([A|B],R,T),
+	less(H,A),
+	checkset([A|B]), 
+	checkset([H|T]),
+	checkset([H|R]).
+union([H|B],[H|R],[H|T]) :- 
+	union(B,R,T),
+	checkset([H|B]), 
+	checkset([H|T]),
+	checkset([H|R]).
 
+%some duplicates
+intersection([H|B], [H|D], [H|F]):-
+	intersection(B,D,F),
+	checkset([H|B]),
+	checkset([H|D]),
+	checkset([H|F]).
+intersection([A|B], [C|D], I):-
+	less(A,C),
+	intersection(B,[C|D],I),
+	checkset([A|B]),
+	checkset([C|D]),
+	checkset(I).
+intersection([A|B], [C|D], I):-
+	less(C,A),
+	intersection([A|B],D,I),
+	checkset([A|B]),
+	checkset([C|D]),
+	checkset(I).
 intersection(_,[],[]).
 intersection([],_,[]).
-intersection([A|B],Y,[A|T]):-
-	ismember(A,Y,yes),
-	intersection(B,Y,T),
-	checkset([A|B]),
-	checkset(Y),
-	checkset([A|T]).
-intersection([A|B],Y,T):-
-	ismember(A,Y,no),
-	intersection(B,Y,T),
-	checkset([A|B]),
-	checkset(Y).
+	
 
 
