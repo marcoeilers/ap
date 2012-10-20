9c940ef46cf2a0926091673715585ee2b87b48ab
Thomas Jespersen <laumann.thomas@gmail.com>
2012-10-20 13:00:56 +0200

    Work on the MR library.

--- erlang/mr.erl
+++ erlang/mr.erl
@@ -4,7 +4,7 @@
 %%% Created : Oct 2011 by Ken Friis Larsen <kflarsen@diku.dk>
 %%%-------------------------------------------------------------------
 -module(mr).
-%-export([start/1, stop/1, job/5]).
+-export([start/1, stop/1, job/5]).
 -compile(export_all).
 %%%% Interface
 
@@ -12,6 +12,8 @@ start(N) ->
     {Reducer, Mappers} = init(N),
     {ok, spawn(fun() -> coordinator_loop(Reducer, Mappers) end)}.
 
+status(CPid) ->
+    rpc(CPid, status).
 
 stop(CPid) ->
     rpc(CPid, stop).
@@ -24,15 +26,18 @@ stop(CPid) ->
 %% Initial: Initial value for the coordinator
 %% Data: The data that should be processed
 
-%%job(CPid, MapFun, RedFun, RedInit, Data) -> 
-
+job(CPid, MapFun, RedFun, RedInit, Data) -> 
+    rpc(CPid, {job, MapFun, RedFun, RedInit, Data}).
 
 %%%% Internal implementation
 
 
 init(N) -> Red = spawn(fun reducer_loop/0), %%spawn(?MODULE, reducer_loop, []),
-	   { Red, [spawn(fun() -> mapper_loop(Red, fun(Y) -> Y end) end) || _ <- lists:seq(1,N) ] }.
-    
+	   { Red, [spawn(fun() -> mapper_loop(Red, id()) end) || _ <- lists:seq(1,N) ] }.
+
+%% Function that generates the id function.
+id() ->
+    fun(X) -> X end.
 
 
 %% synchronous communication
@@ -77,8 +82,16 @@ coordinator_loop(Reducer, Mappers) ->
 	    io:format("~p stopping~n", [self()]),
 	    lists:foreach(fun stop_async/1, Mappers),
 	    stop_async(Reducer),
-	    reply_ok(From)
-	%....
+	    reply_ok(From);
+	{From, status} ->
+	    io:format("This is MR coordinator ~p managing ~B mappers.~n", [self(), length(Mappers)]),
+	    reply_ok(From),
+	    coordinator_loop(Reducer, Mappers);
+	{From, {job, MapFun, RedFun, RedInit, Data}} ->
+	    %% Uh, update mappers and reducers, split data and start processing
+	    lists:foreach(fun(M) -> setup_async(M, MapFun) end, Mappers),
+	    reply_ok(From, executed),
+	    coordinator_loop(Reducer, Mappers)
     end.
 
 
@@ -91,6 +104,14 @@ send_loop(Mappers, [Mid|Queue], [D|Data]) ->
 send_loop(_, _, []) -> ok;
 send_loop(Mappers, [], Data) ->
     send_loop(Mappers, Mappers, Data).
+
+
+%% Safer split function - doesn't choke when N > length(List)
+splitAt(N, List) when N =< length(List) ->
+    lists:split(N, List);
+splitAt(_, List) ->
+    {List, []}.
+
 
 
 %%% Reducer
@@ -116,7 +137,12 @@ mapper_loop(Reducer, Fun) ->
 	stop -> 
 	    io:format("Mapper ~p stopping~n", [self()]),
 	    ok;
-	%....
+	{data, D} ->
+	    io:format("Mapper ~p received data ~p", [self(), D]),
+	    mapper_loop(Reducer, Fun);
+	{setup, NewFun} ->
+	    io:format("Mapper ~p received new mapper function~n", [self()]),
+	    mapper_loop(Reducer, NewFun);
 	Unknown ->
 	    io:format("unknown message: ~p~n",[Unknown]), 
 	    mapper_loop(Reducer, Fun)
--- erlang/mr_test.erl
+++ erlang/mr_test.erl
@@ -4,3 +4,19 @@
 test_init_stop() ->
     {ok, CPid} = mr:start(3),
     mr:stop(CPid).
+
+test_sum() ->
+    {ok,MR} = mr:start(3),
+    {ok,Sum} = mr:job(MR,
+		      fun(X) -> X end,
+		      fun(X, Acc) -> X+Acc end,
+		      0,
+		      lists:seq(1,10)),
+    {ok,Fac} = mr:job(MR,
+		      fun(X) -> X end,
+		      fun(X,Acc) -> X*Acc end,
+		      1,
+		      lists:seq(1,10)),
+    mr:stop(MR),
+    {Sum, Fac}.
+			      
